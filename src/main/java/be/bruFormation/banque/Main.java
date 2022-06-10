65b3964c5f2e6a480a2dd3f2dfa484603736ff24
Ricardo <rutabare.d@gmail.com>
2022-06-10 14:07:03 +0200

    add db

--- src/main/java/be/bruFormation/banque/Main.java
+++ src/main/java/be/bruFormation/banque/Main.java
@@ -4,10 +4,20 @@ import be.bruFormation.banque.models.Bank;
 import be.bruFormation.banque.models.CurrentAccount;
 import be.bruFormation.banque.models.Holder;
 
+import java.sql.*;
+
 public class Main {
-    public static void main(String[] args) {
-        Bank b = new Bank("BNP");
-        Holder t;
-        CurrentAccount cc;
+    private static final String DB_PATH = Main.class.getClassLoader().getResource("dbslide.sqlite3").toString();
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("org.sqlite.JDBC");
+        //System.out.println(path);
+        Connection connection = DriverManager.getConnection("jdbc:sqlite:" + DB_PATH);
+        PreparedStatement statement = connection.prepareStatement("SELECT * FROM student");
+        //statement.setInt(1,1020);
+        ResultSet resultSet = statement.executeQuery();
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString("first_name"));
+        }
     }
 }
